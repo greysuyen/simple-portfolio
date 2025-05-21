d1d78ec4a115c282ae672a48291ba5d6cca123d2
greysuyen <carlambay687@gmail.com>
2025-05-21 17:11:20 +0800

    style changed

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import datetime
 
 def index(request):
-    now = datetime.datetime.now()
+    now = datetime.datetime.now().strftime("%B %d, %Y")
     return render(request, 'main/index.html', {
         "main" : now
     })
